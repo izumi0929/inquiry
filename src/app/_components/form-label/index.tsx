bfc76614e4ea14ab0bb3c6bda93e3e0eedbc052f
izumeeeel <izumi.watahiki@gmail.com>
2023-05-21 12:13:13 +0900

    feat: FormLabel追加

---
+++ src/app/_components/form-label/index.tsx
@@ -0,0 +1,19 @@
+import { forwardRef } from "react"
+
+import styles from "./index.module.css"
+
+type Props = {
+  children: React.ReactNode
+  required: boolean
+}
+
+export const FormLabel = forwardRef<HTMLLabelElement, Props>((props, ref) => (
+  <div className={styles.labelWrapper}>
+    <label className={styles.label} {...props} ref={ref}>
+      {props.children}
+    </label>
+    {props.required && <span className={styles.label_required}>必須</span>}
+  </div>
+))
+
+FormLabel.displayName = "Button"
