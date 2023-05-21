ed230fe83375d7617e9b0588e8d50001ad0441ee
izumeeeel <izumi.watahiki@gmail.com>
2023-05-21 13:58:21 +0900

    feat: InputField追加

---
+++ src/app/inquiry/_components/form-input-field/index.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react"
+import { UseFormRegister } from "react-hook-form"
+
+import { FormInput } from "@/app/_components/form-input"
+import { FormLabel } from "@/app/_components/form-label"
+import {
+  FormItem,
+  FormValues
+} from "@/app/inquiry/_components/inquiry-form/type"
+
+type Props = {
+  formItem: FormItem
+  errorMessage?: string
+  register: UseFormRegister<FormValues>
+}
+
+export const FormInputField: FC<Props> = ({
+  formItem,
+  register,
+  errorMessage
+}) => {
+  return (
+    <div>
+      <FormLabel required>{formItem.label}</FormLabel>
+      <FormInput
+        type={formItem.type}
+        {...register(formItem.name, { required: formItem.required })}
+        errorMessage={errorMessage}
+      />
+    </div>
+  )
+}
