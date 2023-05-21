d718549b3c06441752b3e928fec38031cf3f7998
izumeeeel <izumi.watahiki@gmail.com>
2023-05-21 23:18:56 +0900

    feat: buttonのAttributeを渡せるようにする

--- src/app/_components/button/index.tsx
+++ src/app/_components/button/index.tsx
@@ -1,11 +1,11 @@
-import { forwardRef } from "react"
+import { ButtonHTMLAttributes, forwardRef } from "react"
 
 import styles from "./index.module.css"
 
 type Props = {
   children: React.ReactNode
   type?: "button" | "submit" | "reset"
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
 export const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => (
   <button className={styles.button} {...props} ref={ref}>
