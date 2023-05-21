fd8c93a9507def6e9d6dd46f1f919979d09c3193
izumeeeel <izumi.watahiki@gmail.com>
2023-05-21 12:17:09 +0900

    refactor: class名変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css"
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.headerContainer}>
+      <div className={styles.headerWrapper}>
         <h1 className={styles.title}>お問い合わせフォーム</h1>
       </div>
     </main>
