5130fdb6be294103733892586a419074fc2de30e
izumeeeel <izumi.watahiki@gmail.com>
2023-05-21 17:32:28 +0900

    fix: checkbox修正

--- src/app/_components/form-checkbox/index.tsx
+++ src/app/_components/form-checkbox/index.tsx
@@ -11,15 +11,16 @@ export const FormCheckbox = forwardRef<HTMLInputElement, Props>(
   (props, ref) => (
     <>
       <div className={styles.inputWrapper}>
-      <label className={styles.checkbox} htmlFor={props.name}>
-        <input
-          id={props.name}
-          className={props.errorMessage ? styles.input_error : styles.input}
-          {...props}
-          ref={ref}
-          type="checkbox"
-        />
-        {props.label}</label>
+        <label className={styles.checkbox} htmlFor={props.name}>
+          <input
+            id={props.name}
+            className={props.errorMessage ? styles.input_error : styles.input}
+            {...props}
+            ref={ref}
+            type="checkbox"
+          />
+          <span className={styles.checkbox_icon}>{props.label}</span>
+        </label>
       </div>
       {props.errorMessage && (
         <p className={styles.errorMessage}>{props.errorMessage}</p>
