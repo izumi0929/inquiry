4658e5f85c8ad4379a0f05c232b985c056ccce0c
izumeeeel <izumi.watahiki@gmail.com>
2023-05-24 21:47:31 +0900

    feat: Homeに来たら問い合わせページにリダイレクト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import styles from "./page.module.css"
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.headerWrapper}>
-        <h1 className={styles.title}>お問い合わせフォーム</h1>
-      </div>
-    </main>
-  )
+  return redirect("/inquiry")
 }
